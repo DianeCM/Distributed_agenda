ae68df35d5e1ec7d187bd1170563d8dddacbbeca
DianeCM <dianecm00@gmail.com>
2023-06-28 04:02:44 -0400

    fixing ChordNode

--- channel.py
+++ channel.py
@@ -2,6 +2,8 @@ import zmq
 import random, math
 from constChord import * 
 from address import Address
+import json
+import hashlib
 
 class Channel:
 
@@ -11,8 +13,10 @@ class Channel:
     self.MAXPROC = pow(2, nBits)
     self.address = address
 
-  def join(self):
+  def join(self,address):
     newpid = random.choice(list(set([str(i) for i in range(self.MAXPROC)]) - set(self.members.keys())))
+    self.members[newpid] = address
+    print(f'Node {newpid} joined to chord, with address {address}')
     return newpid
 
   def exists(self,pid):
@@ -22,11 +26,29 @@ class Channel:
     assert pid in self.members.keys(), ''
     self.members.keys.remove(pid) 
 
+  def hash_key(key: str) -> int:
+    """
+    Función de hash que calcula el hash SHA-1 de una cadena de caracteres y devuelve un número entero que se utiliza como la clave del nodo en la red Chord.
+    """
+    sha1 = hashlib.sha1(key.encode('utf-8'))
+    hash_value = int(sha1.hexdigest(), 16) # convierte el hash SHA-1 en un número entero
+    return hash_value
+
+  
   def run(self):
+
     context = zmq.Context()
     socket = context.socket(zmq.REP)
     socket.bind(str(self.address))
-    message = socket.recv().decode()
+
+    message = socket.recv()
+    data=json.loads(message.decode("utf-8"))
+    message=data["message"]
+    address = data["address"]
     print("Received request: %s" % message)
+
     if message == JOIN:
-      socket.send_string(str(self.join()))        
+      data = {"nodeID": self.join(address), "nBits": self.nBits}
+      message = json.dumps(data).encode("utf-8")
+      socket.send(message)
+      #socket.send_string(str(self.join(address)))        
--- chord.py
+++ chord.py
@@ -3,24 +3,95 @@ import channel #-
 import random, math #-
 from constChord import * #-
 from address import Address
-
+import json 
 class ChordNode:
+
     def __init__(self,chan_address,address):
         self.address=address
         self.chan_address = chan_address
-        
+        self.nodeSet = []                           # Nodes discovered so far
+        self.nodeID = None
+        self.nBits = None
+        self.FT = None
+        self.node_address = {}
+    
+    #ok
+    def inbetween(self, key, lwb, upb):                                         
+        if lwb <= upb:                                                            
+            return lwb <= key < upb                                                                                                         
+        return (lwb <= key and key < upb + self.MAXPROC) or (lwb <= key + self.MAXPROC and key < upb)                        
+    
+    def addNode(self, nodeID):                                                  
+        self.nodeSet.append(int(nodeID))                                         
+        self.nodeSet = list(set(self.nodeSet))                                    
+        self.nodeSet.sort()         
 
+    def delNode(self, nodeID):                                                  
+        assert nodeID in self.nodeSet, ''                                         
+        del self.nodeSet[self.nodeSet.index(nodeID)]                              
+        self.nodeSet.sort() 
+
+    def finger(self, i):
+        succ = (self.nodeID + pow(2, i-1)) % self.MAXPROC    # succ(p+2^(i-1))
+        lwbi = self.nodeSet.index(self.nodeID)               # own index in nodeset
+        upbi = (lwbi + 1) % len(self.nodeSet)                # index next neighbor
+        for k in range(len(self.nodeSet)):                   # go through all segments
+            if self.inbetween(succ, self.nodeSet[lwbi]+1, self.nodeSet[upbi]+1):
+                return self.nodeSet[upbi]                        # found successor
+            (lwbi,upbi) = (upbi, (upbi+1) % len(self.nodeSet)) # go to next segment
+        return None                                                                
+
+    def recomputeFingerTable(self):
+        self.FT[0]  = self.nodeSet[self.nodeSet.index(self.nodeID)-1] # Predecessor
+        self.FT[1:] = [self.finger(i) for i in range(1,self.nBits+1)] # Successors
+
+    #ok
+    def localSuccNode(self, key): 
+        if self.inbetween(key, self.FT[0]+1, self.nodeID+1): # key in (FT[0],self]
+            return self.nodeID                                 # node is responsible
+        elif self.inbetween(key, self.nodeID+1, self.FT[1]): # key in (self,FT[1]]
+            return self.FT[1]                                  # successor responsible
+        for i in range(1, self.nBits+1):                     # go through rest of FT
+            if self.inbetween(key, self.FT[i], self.FT[(i+1) % self.nBits]):
+                return self.FT[i]                                # key in [FT[i],FT[i+1])
+    
+    #ok
     def run(self):
         print("Connecting to Channel Server")
         context = zmq.Context()
-        self.socket = context.socket(zmq.REQ)
+        socket = context.socket(zmq.REQ)
         #self.socket.bind(str(self.address))
-        self.socket.connect(str(self.chan_address))
+        socket.connect(str(self.chan_address))
+        
+        #Send a message to join to the network
         print("Sending Message")
-        self.socket.send_string(JOIN)
+        data = {"message": JOIN, "address": str(self.address)}
+        message = json.dumps(data).encode("utf-8")
+        socket.send(message)
+
         #  Get the reply.
-        message = self.socket.recv().decode()
-        print("Joined to the channel as node %s" % (message))
+        message = socket.recv()
+        data=json.loads(message.decode("utf-8"))
+        nodeID=data["nodeID"]
+        nBits = data["nBits"]
+        print("Joined to an %s chord network as node %s" % (nBits,nodeID))
+        self.nodeID = nodeID 
+        self.nBits = nBits
+        self.FT = [None for i in range(self.nBits+1)] # FT[0] is predecessor #-
+
+        #Receiving requests
+        while True:
+            message = socket.recv()
+            data=json.loads(message.decode("utf-8"))
+            request = data["message"]
+            address = data["address"]
+            if request[0] == STOP: 
+                break 
+            if request[0] == LOOKUP_REQ:                       # A lookup request #-
+                nextID = self.localSuccNode(request[1])          # look up next node #-
+                data = {"message": (LOOKUP_REQ,request[1]), "address": str(self.node_address[nextID])} # send to succ #-
+                if not self.chan.exists(nextID):
+                    self.delNode(nextID) 
 
 node= ChordNode(Address("localhost","5555"),Address("localhost","5000"))
 node.run()
